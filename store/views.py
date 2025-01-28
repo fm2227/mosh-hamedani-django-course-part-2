0da3c3daf0266738f6a0eec16c174ef8f7c83511
Farshad Mozafari <farshadmozafari.64@gmail.com>
2025-01-28 14:12:50 +0330

    11- Serializing Relationships(Hyperlinked)

--- store/views.py
+++ store/views.py
@@ -3,14 +3,15 @@ from django.http import HttpResponse
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
 from rest_framework import status
-from .models import Product
-from .serializers import ProductSerializer
+from .models import Product, Collection
+from .serializers import ProductSerializer, CollectionSerializer
 
 
 @api_view()
 def product_list(request):
     queryset = Product.objects.select_related('collection').all()
-    serializer = ProductSerializer(queryset, many=True)
+    serializer = ProductSerializer(
+        queryset, many=True, context={'request': request})
     return Response(serializer.data)
 
 
@@ -19,3 +20,8 @@ def product_detail(request, id):
     product = get_object_or_404(Product, pk=id)
     serializer = ProductSerializer(product)
     return Response(serializer.data)
+
+
+@api_view()
+def collection_detail(request, pk):
+    return Response('ok')
